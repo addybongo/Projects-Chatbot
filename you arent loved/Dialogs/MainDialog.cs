6e3ba3bf73bcbdd4bbf7d5898d6e431210c700ba
addybongo <adnan.ahmad@ntlworld.com>
2020-02-04 17:46:48 +0000

    clean template

---
+++ you arent loved/Dialogs/MainDialog.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using MySql.Data.MySqlClient;
+
+namespace prototypeVer1.Dialogs
+{
+    public class MainDialog : ComponentDialog
+    {
+        protected readonly BotState _UserState;
+        static string name;
+
+        public MainDialog()
+            : base(nameof(MainDialog))
+        {
+
+            var waterfallSteps = new WaterfallStep[]
+            {
+                commandAsync,
+                projectsAsync,
+                confirmAsync,
+                finalAsync
+            };
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private static async Task<DialogTurnResult> commandAsync(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
+        {
+            var promptOptions = new PromptOptions { Prompt = MessageFactory.Text("Hi there! Please type in the organization you wish to search for.") };
+
+            return await stepcontext.PromptAsync(nameof(TextPrompt), promptOptions, cancellationtoken);
+        }
+
+        private static async Task<DialogTurnResult> projectsAsync(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
+        {
+            var response = (String)stepcontext.Result;
+            sqlConnector connector = new sqlConnector();
+
+            var searchingMessage = MessageFactory.Text("Ok, I am searching for projects matching your description...");
+            await stepcontext.Context.SendActivityAsync(searchingMessage, cancellationtoken);
+
+            // List<String>[] results = connector.Select("SELECT * FROM Projects WHERE organizationName='" + response + "';");
+            List<String>[] results = connector.Select("SELECT * FROM Projects WHERE organizationName LIKE'%" + response + "%';");
+
+            if(results[0].Count == 0)
+            {
+                var missingMessage = MessageFactory.Text("Sorry, I couldn't find any projects with this organization!");
+                await stepcontext.Context.SendActivityAsync(missingMessage, cancellationtoken);
+                return await stepcontext.EndDialogAsync(null, cancellationtoken);
+            }
+
+            var foundMessage = MessageFactory.Text("Found " + results[0].Count + " results:");
+            await stepcontext.Context.SendActivityAsync(foundMessage, cancellationtoken);
+
+            for (int i = 0; i < results[0].Count; i++)
+            {
+                var projectEntry = (i + 1) + ". Project Title: " + results[0][i] + " led by " + results[1][i];
+                var projectEntryMessage = MessageFactory.Text(projectEntry, projectEntry);
+                await stepcontext.Context.SendActivityAsync(projectEntryMessage, cancellationtoken);
+            }
+            return await stepcontext.ContinueDialogAsync(cancellationtoken);
+        }
+        private static async Task<DialogTurnResult> confirmAsync(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
+        {
+
+            var promptOptions = new PromptOptions { Prompt = MessageFactory.Text("Did this answer your question?") };
+
+            return await stepcontext.PromptAsync(nameof(ConfirmPrompt), promptOptions, cancellationtoken);
+
+            return await stepcontext.EndDialogAsync(null, cancellationtoken);
+        }
+
+        private static async Task<DialogTurnResult> finalAsync(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
+        {
+
+            var response = (bool)stepcontext.Result;
+
+            if(response)
+            {
+                var foundMessage = MessageFactory.Text("I'm glad I could help!");
+                await stepcontext.Context.SendActivityAsync(foundMessage, cancellationtoken);
+            }
+            else
+            {
+                var foundMessage = MessageFactory.Text("I'm sorry I couldn't help! Try searching again with different keywords");
+                await stepcontext.Context.SendActivityAsync(foundMessage, cancellationtoken);
+            }
+            return await stepcontext.EndDialogAsync(null, cancellationtoken);
+        }
+    }
+}
---
+++ you arent loved/sqlConnector.cs
@@ -0,0 +1,93 @@
+﻿﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prototypeVer1
+{
+    public class sqlConnector
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public sqlConnector()
+        {
+            server = "51.145.112.189";
+            database = "RCGP_Projects";
+            uid = "rcgpadmin";
+            password = "rcgp!12345678";
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("AN ERROR OCCURED IN SQL");
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+
+        public List<string>[] Select(String query)
+        {
+            //Create a list to store the result
+            List<string>[] list = new List<string>[2];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["projectTitle"] + "");
+                    list[1].Add(dataReader["contactName"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+    }
+}
